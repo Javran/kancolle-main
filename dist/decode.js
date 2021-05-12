9c5d5024513fde3f5b7609a5e306ae82ea4c6271
Javran Cheng <Javran.C@gmail.com>
2021-05-11 22:26:39 -0700

    does babel parse?

--- dist/decode.js
+++ dist/decode.js
@@ -54,28 +54,11 @@ var _0x42cb = function(_0x105fb8, _0x3634ac) {
 };
 const { readFileSync, writeFileSync } = require('fs')
 
-const vars = {}
-let i = 0
-
 const decoderFunction = process.argv[2]
 
 writeFileSync(
   'dist/main.stage1.js',
   readFileSync('dist/main.raw.js')
     .toString()
-    .replace(new RegExp(`${decoderFunction}\\('(.+?)'\\)`, 'g'), x => `'${eval(x)}'`.replace(/\n/g, '\\n'))
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/(\S)\['([_a-zA-Z][_a-zA-Z0-9]*?)'\]/g, '$1.$2')
-    .replace(/\[\('([_a-zA-Z][_a-zA-Z0-9]*?)'\)\]/g, "['$1']")
-    .replace(/_(0x[a-f0-9]{4,6})/g, (_, x) => `_${(vars[x] || (vars[x] = ++i)).toString(36)}`)
-    .replace(/(0x[a-f0-9]+?)\.toString()/g, (_, x) => `(${+x}).toString()`)
-    .replace(/(0x[a-f0-9]+)/g, (_, x) => `${+x}`),
+    .replace(new RegExp(`${decoderFunction}\\('(.+?)'\\)`, 'g'), x => `'${eval(x)}'`.replace(/\n/g, '\\n')),
 )
